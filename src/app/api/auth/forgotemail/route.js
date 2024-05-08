5dcd4f97b0de610dfc66b4d5b52b1d5ea86962b2
rEjiN2 <rejin374@gmail.com>
2024-05-08 13:57:38 +0530

    ckecking email

--- src/app/api/auth/forgotemail/route.js
+++ src/app/api/auth/forgotemail/route.js
@@ -21,57 +21,50 @@ function generateRandomPassword(length = 10) {
   
 
 
-export const POST = async(request)=>{
+  export const POST = async (request) => {
     try {
-        await connect()
-       const {email}= await request.json()
+        await connect();
+        const { email } = await request.json();
 
-       const isEmail = await User.findOne({email:email})
+        const isEmail = await User.findOne({ email: email });
 
-       if(isEmail){
+        if (isEmail) {
+            const newPassword = generateRandomPassword();
+            const hashedPassword = await bcrypt.hash(newPassword, 10);
 
+            const courier = new CourierClient({ authorizationToken: process.env.NEXT_COURIER_TOKEN });
 
+            try {
+                const { requestId } = await courier.send({
+                    message: {
+                        content: {
+                            title: "Welcome to BullandBearz!",
+                            body: `Email: ${email}\nPassword: ${newPassword}`
+                        },
+                        data: {
+                            email: email,
+                            joke: newPassword
+                        },
+                        to: {
+                            email: email
+                        }
+                    }
+                });
 
-const newPassword = generateRandomPassword();
-const hashedPassword = await bcrypt.hash(newPassword, 10);
-try{
-    const courier = new CourierClient(
-        { authorizationToken: process.env.NEXT_COURIER_TOKEN});
-
-      const { requestId } = await  courier.send({
-        message: {
-          content: {
-            title: "Welcome to BullandBearz!",
-            body : " Email :{{email}}\nPassword : {{joke}}"
-          },
-          data: {
-            email:email,
-            joke:  newPassword 
-          },
-          to: {
-            email: email
-          }
+                const updatePassword = await User.updateOne({ _id: isEmail._id }, { $set: { password: hashedPassword } });
+                if (updatePassword) {
+                    return new NextResponse(JSON.stringify("Password send to email"), { status: 200 });
+                } else {
+                    return new NextResponse(JSON.stringify("Password send to email but password Not updated"), { status: 301 });
+                }
+            } catch (err) {
+                console.log(err.message);
+                return new NextResponse(JSON.stringify("Error sending email"), { status: 500 });
+            }
+        } else {
+            return new NextResponse(JSON.stringify("Email Not Found"), { status: 404 });
         }
-      });
-       console.log("email send");
-   const updatePassord = await User.updateOne({ _id: isEmail._id }, { $set: { password: hashedPassword } });
-   if(updatePassord){
-       return new NextResponse(JSON.stringify("Password send to email"),{status:200})
-   }else{
-    return new NextResponse(JSON.stringify("Password send to email but password Not updated"),{status:301})
-   }
-}catch(err){
-    console.log(err.message);
-}
-       }
-       else
-       {
-return new NextResponse(JSON.stringify("Email Not Found"),{status:404})
-
-       }
-
     } catch (error) {
-return new NextResponse(JSON.stringify(error.message),{status:500})
-        
+        return new NextResponse(JSON.stringify(error.message), { status: 500 });
     }
 }
