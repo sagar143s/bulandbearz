83f753392fdaadac53e362c1745ac09e47579278
Bull-Bearz <156590187+Bull-Bearz@users.noreply.github.com>
2024-10-02 01:42:57 +0400

    Update EnglishHeader.jsx

--- src/components/English/Navbar/EnglishHeader.jsx
+++ src/components/English/Navbar/EnglishHeader.jsx
@@ -534,6 +534,7 @@ const EnglishHeader = () => {
    {/* <Link href={language === 'english' ? '/contact' :'/contact'} style={{textDecoration:'none'}}><Typography   fontWeight='500' fontSize='15px' color='#fff' sx={{border:'1px solid rgba(44, 62, 80, 0.5)',paddingLeft:'0.5rem',paddingRight:'0.5rem',paddingTop:'0.2rem',paddingBottom:'0.2rem',cursor:'pointer',borderRadius:'8px','&:hover':{background:'#fff',color:'#000'}}}>تواصل معنا</Typography></Link> */}
    </Box>
     }
+     
 
    
 
